33bca34ac7dc040a7ef389969879872d47e3cb49
Yilchik <yulyayulia2@gmail.com>
2024-05-10 21:48:57 +0200

    friend json

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import userData from "./userData.json";
 import "./App.css";
+import userData from "./userData.json";
 import Profile from "./components/Profile";
 import FriendList from "./components/FriendList";
 import friends from "./friends.json";
--- src/components/FriendListItem.jsx
+++ src/components/FriendListItem.jsx
@@ -2,8 +2,8 @@ const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <div>
       <img src={avatar} alt="Avatar" width="48" />
-      <p>Friend {name}</p>
-      <p>Friend {isOnline}</p>
+      <p>{name}</p>
+      <p>{isOnline}</p>
     </div>
   );
 };
